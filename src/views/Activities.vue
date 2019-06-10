f87651802e8e02072b35c4867afc2ea9f2994776
Dario Zubaray <dzubaray@gmail.com>
2019-06-10 09:18:52 -0300

    Agregando el componente de formulario de informacion personal

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -1,5 +1,54 @@
 <template>
     <div class="mt-5">
-        Actividades
+        <b-form @submit.prevent="submitActivity" class="mb-5">
+            <activity-personal-data :personal_data="activity.personal_data"></activity-personal-data>
+            <b-row>
+                <b-col>
+                    <b-button variant="primary" type="submit">Submit</b-button>
+                </b-col>
+            </b-row>
+        </b-form>
     </div>
 </template>
+
+<script>
+import ActivityPersonalData from '../components/ActivityForm/ActivityPersonalData'
+import validationMixin from '../mixins/validation'
+
+export default {
+    mixins: [
+        validationMixin
+    ],
+    components: {
+        ActivityPersonalData
+    },
+    data() {
+        return {
+          reset: true,
+          activity_types_components: [
+            'basket', 'football', 'tennis'
+          ],
+          activity: {
+            terms: false,
+            personal_data: {
+              name: '',
+              surname: ''
+            },
+            activity_type_selection: {
+              type: 1,
+              date: ''
+            },
+            activity_selected_data: {
+              team: '',
+              information: ''
+            }
+          }
+        }
+    },
+    methods: {
+        async submitActivity() {
+            const validate = await this.$validator.validateAll()
+        }
+    }
+}
+</script>
