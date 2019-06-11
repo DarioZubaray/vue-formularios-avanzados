c9f61e5477122afefa738648d912143552a55489
Dario Zubaray <dzubaray@gmail.com>
2019-06-11 08:29:48 -0300

    Carga de componentes dinámicos a través de campo de selección

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -3,6 +3,10 @@
         <b-form @submit.prevent="submitActivity" class="mb-5">
             <activity-personal-data :personal_data="activity.personal_data"></activity-personal-data>
             <activity-type-selection :activity_type_selection="activity.activity_type_selection"></activity-type-selection>
+            <component
+                :is="activityComponent"
+                :activity_selected_data="activity.activity_selected_data"
+            ></component>
             <b-row>
                 <b-col>
                     <b-button variant="primary" type="submit">Submit</b-button>
@@ -15,6 +19,9 @@
 <script>
 import ActivityPersonalData from '../components/ActivityForm/ActivityPersonalData'
 import ActivityTypeSelection from '../components/ActivityForm/ActivityTypeSelection'
+import Basket from '../components/ActivityForm/Types/Basket'
+import Football from '../components/ActivityForm/Types/Football'
+import Tennis from '../components/ActivityForm/Types/Tennis'
 import validationMixin from '../mixins/validation'
 
 export default {
@@ -22,7 +29,8 @@ export default {
         validationMixin
     ],
     components: {
-        ActivityPersonalData, ActivityTypeSelection
+        ActivityPersonalData, ActivityTypeSelection,
+        Basket, Football, Tennis
     },
     data() {
         return {
@@ -46,6 +54,24 @@ export default {
             }
           }
         }
+    },
+    computed: {
+      activityComponent () {
+        switch (this.activity.activity_type_selection.type) {
+          case 1: {
+            return 'football'
+          }
+          case 2: {
+            return 'basket'
+          }
+          case 3: {
+            return 'tennis'
+          }
+          default: {
+            return 'football'
+          }
+        }
+      }
     },
     methods: {
         async submitActivity() {
